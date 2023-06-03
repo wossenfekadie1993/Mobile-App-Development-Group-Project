fafae913fbe7408da788d4833df9e3f6192f3a56
YabsiraYetwale <119517823+YabsiraYetwale@users.noreply.github.com>
2023-06-03 08:00:22 +0300

    Add files via upload

---
+++ frontend/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_3.7.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Admin\\Desktop\\MOBILE-APP-DEV\\students_voice" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_3.7.5-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Admin\\Desktop\\MOBILE-APP-DEV\\students_voice"
+  "FLUTTER_ROOT=C:\\flutter_windows_3.7.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Admin\\Desktop\\MOBILE-APP-DEV\\students_voice\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Admin\\Desktop\\MOBILE-APP-DEV\\students_voice"
+  "FLUTTER_TARGET=C:\\Users\\Admin\\Desktop\\MOBILE-APP-DEV\\students_voice\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Admin\\Desktop\\MOBILE-APP-DEV\\students_voice\\.dart_tool\\package_config.json"
+)
